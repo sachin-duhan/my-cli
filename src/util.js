064229686a1da13a152a54a9075ac5dd8f8efba0
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-30 06:18:26 +0530

    updating CP template

--- src/util.js
+++ src/util.js
@@ -15,9 +15,9 @@ using namespace __gnu_pbds;
 
 typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
-#define loop(i, k, n) for (i = k; i <= n; ++i)
-#define rloop(i, k, n) for (i = k; i >= n; --i)
-#define FAST_IO                   \
+#define loop(i, k, n) for (auto i = k; i <= n; ++i)
+#define rloop(i, k, n) for (auto i = k; i >= n; --i)
+#define FAST_IO \
 ios_base::sync_with_stdio(0); \
 cin.tie(0);                   \
 cout.tie(0);
